ce61857b6e01086f4f8c82c9423e1b9b9922326f
Brian McFee <brm2132@columbia.edu>
2013-08-22 15:43:12 -0400

    rearranging code

--- code/make_salami_train.py
+++ code/make_salami_train.py
@@ -1,13 +1,12 @@
 import numpy as np
 import glob
 import os
+import sys
+
 from joblib import Parallel, delayed
 import cPickle as pickle
 
 from segmenter import features
-
-DATA_PATH = '/home/bmcfee/git/olda/data/features/'
-
 
 def align_segmentation(filename, beat_times):
     '''Load a ground-truth segmentation, and align times to the nearest detected beats
@@ -41,8 +40,8 @@ def get_annotation(song, rootpath):
 
 # <codecell>
 
-def import_data(song, rootpath):
-        data_file = '%s/%s.pickle' % (DATA_PATH, os.path.splitext(os.path.basename(song))[0])
+def import_data(song, rootpath, output_path):
+        data_file = '%s/%s.pickle' % (output_path, os.path.splitext(os.path.basename(song))[0])
 
         if os.path.exists(data_file):
             with open(data_file, 'r') as f:
@@ -71,13 +70,13 @@ def import_data(song, rootpath):
 
 # <codecell>
 
-def make_dataset(n=None, n_jobs=4, rootpath='/home/bmcfee/data/SALAMI/'):
+def make_dataset(n=None, n_jobs=4, rootpath='SALAMI/', output_path='data/'):
     
     files = sorted(filter(lambda x: os.path.exists(get_annotation(x, rootpath)), glob.glob('%s/audio/*.mp3' % rootpath)))
     if n is None:
         n = len(files)
 
-    data = Parallel(n_jobs=n_jobs)(delayed(import_data)(song, rootpath) for song in files[:n])
+    data = Parallel(n_jobs=n_jobs)(delayed(import_data)(song, rootpath, output_path) for song in files[:n])
     
     X, Y, B, T, F = [], [], [], [], [], []
     for d in data:
@@ -93,6 +92,8 @@ def make_dataset(n=None, n_jobs=4, rootpath='/home/bmcfee/data/SALAMI/'):
 
 
 if __name__ == '__main__':
-    X, Y, B, T, F = make_dataset()
-    with open('/home/bmcfee/Desktop/segment_data.pickle', 'w') as f:
+    salami_path = sys.argv[1]
+    output_path = sys.argv[2]
+    X, Y, B, T, F = make_dataset(rootpath=salami_path, output_path=output_path)
+    with open('%s/segment_data.pickle' % output_path, 'w') as f:
         pickle.dump( (X, Y, B, T, F), f)
