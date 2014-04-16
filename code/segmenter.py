cebbc3d4aa9b38871ff3d49ba90076fc7ea1d515
Brian McFee <brm2132@columbia.edu>
2014-04-16 14:15:08 -0400

    so many segment names...

--- code/segmenter.py
+++ code/segmenter.py
@@ -51,10 +51,10 @@ __DIMENSION = N_MFCC + N_CHROMA + 2 * N_REP + 4
 LABEL_K     = 3
 
 SEGMENT_NAMES = list(string.ascii_uppercase)
-SEGMENT_NAMES.extend(['A%s' % x for x in string.ascii_lowercase])
-SEGMENT_NAMES.extend(['B%s' % x for x in string.ascii_lowercase])
-SEGMENT_NAMES.extend(['C%s' % x for x in string.ascii_lowercase])
+for x in string.ascii_uppercase:
+    SEGMENT_NAMES.extend(['%s%s' % (x, y) for y in string.ascii_lowercase])
 
+print SEGMENT_NAMES
 def features(filename):
     '''Feature-extraction for audio segmentation
     Arguments:
