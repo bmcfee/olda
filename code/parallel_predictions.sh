2da8076c9e63dd3465435088dc8fe5b114fb656a
Brian McFee <brm2132@columbia.edu>
2014-04-23 14:13:07 -0400

    fixed the parallel predictor

--- code/parallel_predictions.sh
+++ code/parallel_predictions.sh
@@ -16,11 +16,16 @@ predictor() {
     switch=$5
     file=$6
     prediction="$(basename "$file" .pickle).lab"
-    ./feature_segmenter.py -l ${label} -b ${boundary} ${switch} "${file}" "../data/predictions/${dataset}/${algo}/${prediction}"
+
+    ./feature_segmenter.py  -l ${label} \
+                            -b ${boundary} \
+                            "${file}" \
+                            ${switch} \
+                            "../data/predictions/${dataset}/${algo}/${prediction}" 
 }
 export -f predictor
 
-parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  BEATLES_TUT olda_aic        '' {1} ::: ../data/features/BEATLES_TUT/*.pickle
+parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  BEATLES_TUT olda_aic        -a {1} ::: ../data/features/BEATLES_TUT/*.pickle
 parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  BEATLES_TUT olda_spectral   -s {1} ::: ../data/features/BEATLES_TUT/*.pickle
 parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${LABEL_MODEL}     BEATLES_TUT spectral        -s {1} ::: ../data/features/BEATLES_TUT/*.pickle
 
@@ -29,7 +34,7 @@ parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${LABEL_MODEL}     BEATLES_TUT 
 LABEL_MODEL=../data/label_beatles_tut.npy
 BOUNDARY_MODEL=../data/boundary_beatles_tut.npy
 
-parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  JAMS olda_aic        '' {1} ::: ../data/features/JAMS/*.pickle
+parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  JAMS olda_aic        -a {1} ::: ../data/features/JAMS/*.pickle
 parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${BOUNDARY_MODEL}  JAMS olda_spectral   -s {1} ::: ../data/features/JAMS/*.pickle
 parallel -j ${NUM_JOBS} predictor ${LABEL_MODEL} ${LABEL_MODEL}     JAMS spectral        -s {1} ::: ../data/features/JAMS/*.pickle
 
