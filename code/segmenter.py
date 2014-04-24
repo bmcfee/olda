b0a9e262cf2cf2b0dbf925b0615261faba9b4d92
Brian McFee <brm2132@columbia.edu>
2014-04-23 22:06:21 -0400

    repetition features 2.0

--- code/segmenter.py
+++ code/segmenter.py
@@ -56,6 +56,14 @@ SEGMENT_NAMES = list(string.ascii_uppercase)
 for x in string.ascii_uppercase:
     SEGMENT_NAMES.extend(['%s%s' % (x, y) for y in string.ascii_lowercase])
 
+def rw_laplacian(A):
+    '''Random-walk graph laplacian of a symmetric matrix'''
+
+    Dinv = np.diag(np.sum(A, axis=1)**-1.0)
+    L = np.eye(A.shape[0]) - Dinv.dot(A)
+    return L
+    
+
 def features(filename):
     '''Feature-extraction for audio segmentation
     Arguments:
@@ -79,42 +87,6 @@ def features(filename):
 
     '''
     
-    
-
-    def compress_data(X, k):
-        e_vals, e_vecs = scipy.linalg.eig(X.dot(X.T))
-        
-        e_vals = np.maximum(0.0, np.real(e_vals))
-        e_vecs = np.real(e_vecs)
-        
-        idx = np.argsort(e_vals)[::-1]
-        
-        e_vals = e_vals[idx]
-        e_vecs = e_vecs[:, idx]
-        
-        # Truncate to k dimensions
-        if k < len(e_vals):
-            e_vals = e_vals[:k]
-            e_vecs = e_vecs[:, :k]
-        elif k > len(e_vals):
-            # Pad on zeros so we're k-by-k
-            e_vals = np.pad(e_vals, (0, k - len(e_vals)), mode='constant')
-            e_vecs = np.pad(e_vecs, [(0,0), (0, k - e_vecs.shape[1])], mode='constant')
-            pass
-        
-        # Normalize by the leading singular value of X
-        Z = np.sqrt(e_vals.max())
-        
-        if Z > 0:
-            e_vecs = e_vecs / Z
-        
-        return e_vecs.T.dot(X)
-
-    # Harmonic waveform
-    def harmonify(y):
-        D = librosa.stft(y)
-        return librosa.istft(librosa.decompose.hpss(D)[0])
-
     # HPSS waveforms
     def hpss_wav(y):
         H, P = librosa.decompose.hpss(librosa.stft(y))
@@ -162,26 +134,61 @@ def features(filename):
         return librosa.logamplitude(librosa.util.normalize(C_to_Chr.dot(CQT)))
 
     # Latent factor repetition features
+    def clean_reps(S):
+        # Median filter with reflected padding
+        Sf = np.pad(S, [(0, 0), (REP_WIDTH, REP_WIDTH)], mode='reflect')
+        Sf = scipy.signal.medfilt2d(Sf, kernel_size=(1, REP_WIDTH))
+        Sf = Sf[:, REP_WIDTH:-REP_WIDTH]
+        return Sf
+
+    def laplacian_eigenvectors(L, k):
+        e_vals, e_vecs = scipy.linalg.eig(L)
+        e_vals = e_vals.real
+        idx = np.argsort(e_vals)
+    
+        e_vals = e_vals[idx]
+        e_vecs = e_vecs[:, idx]
+        
+        # Trim the bottom eigenvalue/vector
+        e_vals = e_vals[1:]
+        e_vecs = e_vecs[:, 1:]
+       
+        if k < len(e_vals):
+            e_vals = e_vals[:k]
+            e_vecs = e_vecs[:, :k]
+        elif k > len(e_vals):
+            # Pad on zeros so we're k-by-k
+            e_vals = np.pad(e_vals, (0, k - len(e_vals)), mode='constant')
+            e_vecs = np.pad(e_vecs, [(0,0), (0, k - e_vecs.shape[1])], mode='constant')
+            pass
+
+        
+        return e_vecs[:, :k].T
+    
+    # Latent factor repetition features
     def repetition(X, metric='sqeuclidean'):
         R = librosa.segment.recurrence_matrix(X, 
                                             k=2 * int(np.ceil(np.sqrt(X.shape[1]))), 
                                             width=REP_WIDTH, 
                                             metric=metric,
                                             sym=True).astype(np.float32)
+        
+        S = librosa.segment.structure_feature(R)
+        Sf = clean_reps(S)
+        # De-skew
+        Rf = librosa.segment.structure_feature(Sf, inverse=True)
 
-        R = librosa.segment.structure_feature(R)[1:]
+        # Symmetrize by force
+        Rf = 0.5 * (Rf + Rf.T)
         
-        # Or the repetition matrix with its reverse
-        P = np.maximum(R, R[::-1])[:X.shape[1]]
-        
-        # Median filter to suppress blips
-        P = scipy.signal.medfilt2d(P, [1, REP_FILTER])
-        
-        # Discard empty rows.  
-        # This should give an equivalent SVD, but resolves some numerical instabilities.
-        P = compress_data(P[P.any(axis=1)], N_REP)
-        
-        return P
+        # We can jump to a random neighbor, or +- 1 step in time
+        M = Rf + np.eye(Rf.shape[0], k=1) + np.eye(Rf.shape[0], k=-1)
+
+        # Get the random walk laplacian laplacian
+        L = rw_laplacian(M)
+
+        # Get the bottom k eigenvectors of L
+        return laplacian_eigenvectors(L, k=N_REP)
 
     print '\t[1/6] loading audio'
     # Load the waveform
@@ -343,13 +350,9 @@ def label_build_affinity(X, k, local=False):
 def label_estimate_n_components(A):
     ''' Takes in an affinity matrix and estimates the number of clusters by spectral gap'''
 
-    n = len(A)
-
-    # Build the degree matrix
-    Dinv = A.sum(axis=1)**-1.0
     
     # Build the random-walk graph laplacian
-    L = np.eye(n) - (A * Dinv).T
+    L = rw_laplacian(A)
 
     # Get the spectrum
     spectrum = scipy.linalg.eig(L)[0].real
